c1d982ef01583af7409161365b616e91e24cf343
s0h3ck <s0h3ck@gmail.com>
2018-10-30 20:07:39 -0700

    Add FctAcceptSwitchGroup.sql

---
+++ SQL/FctAcceptSwitchGroup.sql
@@ -0,0 +1,34 @@
+-- DROP FUNCTION tinderroulette.SWITCHGROUP(character, text);
+
+CREATE OR REPLACE FUNCTION tinderroulette.ACCEPT_SWITCHGROUP(
+	cip_requested character,
+	user_cip character,
+	idclass text)
+RETURNS void AS 
+$$
+BEGIN
+	IF EXISTS (
+		SELECT id_group
+		FROM tinderroulette.switchgrouprequest
+		WHERE id_class = idclass
+		AND cip = cip_requested
+	) THEN
+		-- SWITCHGROUP
+		PERFORM tinderroulette.SWITCHGROUP(
+		cip_requested, 
+		user_cip,
+		(SELECT id_group FROM tinderroulette.groupstudent WHERE cip = cip_requested),
+		(SELECT id_group FROM tinderroulette.groupstudent WHERE cip = user_cip)
+		);
+		
+		-- DELETE REQUEST IN SWITCHGROUPREQUEST
+		DELETE FROM tinderroulette.switchgrouprequest
+		WHERE cip = cip_requested
+		AND id_class = id_class;
+	END IF;
+END
+$$
+LANGUAGE plpgsql;
+
+--SELECT tinderroulette.SWITCHGROUP('cotd2511', 'fanv2501', 1, 2); 
+SELECT tinderroulette.ACCEPT_SWITCHGROUP('cotd2511', 'fanv2501', 'S6GEN60002A18');
